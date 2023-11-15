011d273ae1e6caa13a74cf82e4add206d7e85753
Kanyaton Somjit <113116925+Kanyatonsita@users.noreply.github.com>
2023-11-15 13:11:56 +0100

    updateBookRoom

---
+++ functions/updateBookRoom/index.js
@@ -0,0 +1,9 @@
+const AWS = require('aws-sdk');
+const { sendResponse } = require('../../responses');
+const db = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+    const { roomId } = event.pathParameters;
+
+    
+}
