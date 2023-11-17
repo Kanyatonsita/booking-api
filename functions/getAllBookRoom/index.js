2c874d3c229bce32bc28aab458addc81fcf4a8ab
Kanyaton Somjit <113116925+Kanyatonsita@users.noreply.github.com>
2023-11-17 12:28:12 +0100

    funktionsGetAllBookRoom

---
+++ functions/getAllBookRoom/index.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk');
+const { sendResponse } = require('../../responses');
+const db = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+
+    const result = await db.scan({
+        TableName: 'rooms-db'
+      }).promise();
+    
+    try {
+    //Get all booked room
+    // const bookRooms = result.Items.filter(item => item.booked && item.booked.length > 0)
+    const bookRooms = result.Items.filter(item => Array.isArray(item.booked) && item.booked.length > 0);
+    
+    if (bookRooms.length > 0) {
+        return sendResponse(200, { rooms: bookRooms });
+    } else {
+        return sendResponse(404, { message: 'Booking not found' });
+    }
+    } catch (error) {
+        return sendResponse(500, {resultItems: result.Items});
+    }
+}
